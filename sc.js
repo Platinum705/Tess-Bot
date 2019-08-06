2b7ac74c6995c89cba41653178ebe38c0ef090ed
0CherryTea0 <53561510+0CherryTea0@users.noreply.github.com>
2019-08-06 15:38:24 +0500

    Update sc.js

--- sc.js
+++ sc.js
@@ -141,11 +141,12 @@ robot.on('message', message => {
 
 robot.on('message', message => {
     if(message.content.startsWith(p + 'help')) {
-    message.reply("Загрузка команд...");
+    message.channel.send("Загрузка команд...");
 function msdl() {
 message.channel.bulkDelete(1)
 }
 setTimeout(msdl, 1000)
+message.channel.send("Я отправил тебе список команд в личные сообщения)
 
 function helpS() {  
         const embed = new Discord.RichEmbed()
@@ -154,7 +155,7 @@ function helpS() {
             .setDescription('Мои команды \n **tess!help** - команды бота \n **tess!afk on** - войти в AFK \n **tess!afk off** - выйти из AFK \n **tess!logo** - стырить лого сервера \n **tess!avatar** - стырить аву пользователя \n ***Этот список будет дополняться т.к автор ленивая жопа***')
             .setFooter("Tess bot")
             .setTimestamp();
-        message.channel.send({embed}).then(sentMessage => {   
+    robot.users.get(message.author.id).send({embed}).then(sentMessage => {   
             sentMessage.react('🇭')
                 .then(() => sentMessage.react('🇪'))
                     .then(() => sentMessage.react('🇱'))
