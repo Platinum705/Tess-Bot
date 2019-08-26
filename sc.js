dd9fd48dc0f30bf6ce48f81aa98f53f219b5432a
0CherryTea0 <53561510+0CherryTea0@users.noreply.github.com>
2019-08-26 16:47:01 +0500

    Update sc.js

--- sc.js
+++ sc.js
@@ -14,7 +14,8 @@ client.on('ready', () => {
 
 function status1() {
 let UTC = new Date()
-client.user.setActivity(UTC.setHours(0, 0, 0, 0), {
+let UTC2 = UTC.setHours(0, 0, 0, 0),
+client.user.setActivity(UTC2, {
   type: "STREAMING",
   url: "https://www.twitch.tv/monstercat"
 });
