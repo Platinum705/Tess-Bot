8e0065564015a38cfe0312fda0f136253f296aef
0CherryTea0 <53561510+0CherryTea0@users.noreply.github.com>
2019-12-13 09:40:49 +0500

    Update sc.js

--- sc.js
+++ sc.js
@@ -212,7 +212,7 @@ if(message.content.includes(p + "invitebot")) {
 const embed = new Discord.RichEmbed()
             .setTitle("Приглашение")
             .setColor("#00BFFF")
-            .setDescription('Пригласить бота с правами администратора \n https://discordapp.com/api/oauth2/authorize?client_id=466896060682469377&permissions=8&scope=bot \n Пригласить бота без прав администратора \n https://discordapp.com/api/oauth2/authorize?client_id=466896060682469377&permissions=0&scope=bot')
+            .setDescription('Пригласить бота с правами администратора\n https://discordapp.com/api/oauth2/authorize?client_id=466896060682469377&permissions=8&scope=bot\n Пригласить бота без прав администратора\n https://discordapp.com/api/oauth2/authorize?client_id=466896060682469377&permissions=0&scope=bot')
             .setFooter("Tess bot")
             .setTimestamp();
     message.channel.send({embed})
