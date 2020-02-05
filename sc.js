4df7bfb6227de81057cc39bd8185e4eecac7d1b4
0CherryTea0 <53561510+0CherryTea0@users.noreply.github.com>
2020-02-05 09:53:30 +0500

    Update sc.js

--- sc.js
+++ sc.js
@@ -213,7 +213,7 @@ const embed = new Discord.RichEmbed()
 });
 
 
-
+/*
 if(message.content.includes(p + "time")) {
 
 var uakiev = new Date().toLocaleTimeString('ru', {
@@ -246,6 +246,6 @@ minute: 'numeric'
 message.channel.send("Время на Украине: " + uakiev + "\n" + "Время в Ижевске: " + Lisa + "\n" + "Время в Тюмени: " + tym + "\n" + "Время в Москве: " + msc + "\n" + "\n" + "\n" + "Если в данном списке вы не видите своего времени, то обратитесь в личные сообщения к <@405258156063850497> указав свой город и часовой пояс (пример: Мой город Москва, часовой пояс GMT+3)").then((msg) => { 
 
 }
-
+*/
 
 client.login(process.env.BOT_SECRET);
