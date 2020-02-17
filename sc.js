fdd5b813538bb94a1bf20f42fde5b9f6b22b412b
0CherryTea0 <53561510+0CherryTea0@users.noreply.github.com>
2020-02-17 21:07:43 +0500

    Update sc.js

--- sc.js
+++ sc.js
@@ -298,6 +298,13 @@ mentionuser = message.mentions.users.first();
      
    }
     
+    if(message.content.startsWith(p + "clear")) {
+    delmsg = message.content.slice (11)
+message.channel.bulkDelete(delmsg).then(() => {
+  message.channel.send("Deleted" + delmsg + "messages.").then(msg => msg.delete(3000));
+});
+
+  }
      
 }); 
 
