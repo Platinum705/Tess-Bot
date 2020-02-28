940644dbfdff8ddf6db8551a432dfbe897f60bd0
0CherryTea0 <53561510+0CherryTea0@users.noreply.github.com>
2020-02-28 18:43:25 +0500

    Update sc.js

--- sc.js
+++ sc.js
@@ -293,7 +293,7 @@ mentionuser = message.mentions.users.first();
    if(message.content.startsWith(p + "sex")) {
      if (message.channel.nsfw == false) { return message.channel.send("Разрешено только в каналах с nsfw") }
      if (mentionuser == null) { return; }
-     hentaiCount = 7
+     hentaiCount = 9
      hentaiNumber = Math.floor (Math.random() * (hentaiCount - 1 + 1))+1;
      hentaimember = message.author
      hentaidelay = message.content.slice (9)
