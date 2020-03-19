35a1c19a61b7d52f039424c87c03f2c312ee16a8
0CherryTea0 <53561510+0CherryTea0@users.noreply.github.com>
2020-03-19 08:48:53 +0500

    Update sc.js

--- sc.js
+++ sc.js
@@ -309,7 +309,7 @@ if(message.content.startsWith(p + "say2")) {
    if(message.content.startsWith(p + "kick")) {
     message.delete()
     kickCount = 6
-    kickNumber = Math.floor (Math.random() * (hentaiCount2 - 1 + 1))+1;
+    kickNumber = Math.floor (Math.random() * (kickCount - 1 + 1))+1;
     let kickmem2 = message.mentions.users.first()
     let kickmem = message.content.slice(10).split("|")
     message.channel.send(kickmem2 + " **ударил** " + kickmem[1], { files: ["./images/kick/" + kickNumber + ".gif"]} )
