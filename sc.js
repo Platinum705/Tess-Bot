aabf0f64f1574b4b0fdbabb4a67f94f356e53131
0CherryTea0 <53561510+0CherryTea0@users.noreply.github.com>
2020-05-01 22:48:39 +0500

    Update sc.js

--- sc.js
+++ sc.js
@@ -202,37 +202,26 @@ try {
  
 
 if(message.content.startsWith(p + "eval")) {
-
-        //
-        let embed = new Discord.RichEmbed()
-        .setTitle("Evaluation")
-        .setDescription("Sorry, the `eval` command can only be executed by the Developer.")
-        .setColor("#cdf785");
-        if(message.author.id !== '405258156063850497') return message.channel.send(embed);
-        function clean(text) {
-        if (typeof(text) === "string")
-          return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-        else
-            return text;
-      }try {
-            const code = args.join(" ");
-            let evaled = eval(code);
-            let rawEvaled = evaled;
-            if (typeof evaled !== "string")
-              evaled = require("util").inspect(evaled);
-      
-        let embed = new Discord.RichEmbed()
-            .setTitle(`Evaluated in ${Math.round(bot.ping)}ms`)
-            .addField(":inbox_tray: Input", `\`\`\`js\n${code}\n\`\`\``)
-            .addField(":outbox_tray: Output", `\`\`\`js\n${clean(evaled).replace(bot.token, "No no no")}\n\`\`\``)
-            .addField('Type', `\`\`\`xl\n${(typeof rawEvaled).substr(0, 1).toUpperCase() + (typeof rawEvaled).substr(1)}\n\`\`\``)
-            .setColor('GREEN');
-            message.channel.send({embed});
-          } catch (err) {
-            
-            message.channel.send(`\`ERROR\` \`\`\`js\n${clean(err)}\n\`\`\``);
-          }
+        let toEval = args.join(" ");
+        let evaluated = inspect(eval(toEval, { depth: 0 } ))
+        try {
+            if(toEval) {
+                let hrStart = process.hrtime()
+                let hrDiff;
+                hrDiff = process.hrtime(hrStart)
+                return message.channel.send(`*Executed in ${hrDiff[0] > 0 ? `${hrDiff[0]}s` : ''}${hrDiff[1] / 1000000}ms.*\`\`\`javascript\n${evaluated}\n\`\`\``, { maxLength: 1900 })
+                
+            } else {
+                message.channel.send("Error whilst evaluating: `cannot evaluated air`")
+            }
+        } catch(e) {
+            message.channel.send(`Error whilst evaluating: \`${e.message}\``)
+        }
+    } else {
+        return message.reply(" you dont have permission to use this command.").then(m => m.delete(10000))
     }
+
+}
  
 });
 
