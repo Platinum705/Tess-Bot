c726a37b944f07ca13ba9825f27c24c360514dcf
0CherryTea0 <53561510+0CherryTea0@users.noreply.github.com>
2020-05-01 22:28:25 +0500

    Update sc.js

--- sc.js
+++ sc.js
@@ -202,25 +202,29 @@ try {
  
 
 if(message.content.startsWith(p + "eval")) {
-try {
+
+        try {
             let toEval = args.join(" ")
-			let evaluated = inspect(eval(toEval, { depth: 0 }));
-            message.delete()
+            let evaluated = inspect(eval(toEval, { depth: 0 }));
+            
             if (!toEval) {
-                return message.channel.send(`Error while evaluating: Cannot evaluate \`air\``);
+                return message.channel.send(`Error while evaluating: \`air\``);
             } else {
                 let hrStart = process.hrtime()
                 let hrDiff;
                 hrDiff = process.hrtime(hrStart);
-                return message.channel.send(`*Executed in ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ``}${hrDiff[1] / 1000000}ms.*\n\`\`\`javascript\n${evaluated}\n\`\`\``, { maxLength: 1900 })
+                return message.channel.send(`*Executed in ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ''}${hrDiff[1] / 1000000}ms.*\n\`\`\`javascript\n${evaluated}\n\`\`\``, { maxLength: 1900 })
             }
             
         } catch (e) {
-            console.log(e)
             return message.channel.send(`Error while evaluating: \`${e.message}\``);
-        }
+        
 
-      } 
+      } else {
+        return message.reply(" you are not the bot owner!").then(msg => msg.delete(5000))
+      }
+   
+}
 
 });
 
