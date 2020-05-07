ea8636ba91c5856e7f338d755111f0d404b62c5e
0CherryTea0 <53561510+0CherryTea0@users.noreply.github.com>
2020-05-07 21:40:55 +0500

    Update sc.js

--- sc.js
+++ sc.js
@@ -213,7 +213,7 @@ client.on('message', message => {
    if(message.content.startsWith(p + "give")) {
     let rolename = message.content.split(" ");
     let usermention = message.content.split(" ");
-     client.guild.createRole({
+     client.createRole({
       name: rolename[2],
    })
     .then(() => client.users.get(usermention[1]).addRole(rolename[2]) );
