4a26ea72a4d3603f805ae485c75c1b5080fd4728
0CherryTea0 <53561510+0CherryTea0@users.noreply.github.com>
2020-05-10 17:59:06 +0500

    Update sc.js

--- sc.js
+++ sc.js
@@ -267,8 +267,8 @@ client.on("message", message => {
 client.on('message', message => {
  if(message.content.startsWith(p + "role")) {
  let reactCount = 2
-  message.channel.send("Test").then(() => { message.react('🔙') })
-   if (reaction.emoji.name === '🔙' && reaction.count = reactCount ) {
+  message.channel.send("Test").then(() => { message.react('👎') })
+   if (reaction.emoji.name === '👎' && reaction.count = reactCount ) {
     message.channel.send("test")
   }
  }
